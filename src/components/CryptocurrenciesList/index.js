86b29b5c8bfa0ca4f68556d476518fdce8f7d33d
NaveenKomara07 <naveenkomara0525@gmail.com>
2023-06-26 04:29:54 +0000

    solution

---
+++ src/components/CryptocurrenciesList/index.js
@@ -0,0 +1,50 @@
+import {Component} from 'react'
+
+import CryptocurrencyItem from '../CryptocurrencyItem'
+
+import './index.css'
+
+class CryptocurrenciesList extends Component {
+  renderCryptocurrenciesHeader = () => (
+    <div className="list-header">
+      <p className="coin-heading">Coin Type</p>
+      <div className="list-container">
+        <p className="coin-heading-2">USD</p>
+        <p className="coin-heading-2">EURO</p>
+      </div>
+    </div>
+  )
+
+  renderCryptocurrenciesView = () => {
+    const {cryptocurrenciesData} = this.props
+
+    return (
+      <div className="crypto-container" data-testid="loader">
+        {this.renderCryptocurrenciesHeader()}
+        <ul className="cryptocurrencies-list">
+          {cryptocurrenciesData.map(eachCryptocurrency => (
+            <CryptocurrencyItem
+              key={eachCryptocurrency.id}
+              cryptocurrencyDetails={eachCryptocurrency}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="cryptocurrencies-container" data-testid="loader">
+        <h1 className="heading">Cryptocurrency Tracker</h1>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
+          alt="cryptocurrency"
+        />
+        {this.renderCryptocurrenciesView()}
+      </div>
+    )
+  }
+}
+
+export default CryptocurrenciesList
---
+++ src/components/CryptocurrencyItem/index.js
@@ -0,0 +1,30 @@
+import './index.css'
+
+const CryptocurrencyItem = props => {
+  const {cryptocurrencyDetails} = props
+  const {
+    currencyLogoUrl,
+    currencyName,
+    usdValue,
+    euroValue,
+  } = cryptocurrencyDetails
+
+  return (
+    <li className="cryptocurrency-item">
+      <div className="logo-container" data-testid="loader">
+        <img
+          src={currencyLogoUrl}
+          className="currency-logo"
+          alt={currencyName}
+        />
+        <p className="currency-name">{currencyName}</p>
+      </div>
+      <div className="value-container">
+        <p className="currency-value">{usdValue}</p>
+        <p className="currency-value">{euroValue}</p>
+      </div>
+    </li>
+  )
+}
+
+export default CryptocurrencyItem
